c3df1fccc89ad690467ce7dc66495441f41c7e41
Tomvd682 <146827201+Tomvd682@users.noreply.github.com>
2025-10-17 03:10:04 +0200

    UC19: Producten uit DB + nieuw product aanmaken
    SQLite repo; ProductService Add/Get/Delete; NewProductView(Model); route + toolbar; lijst refresht na opslaan.

--- Grocery.App/Views/ProductView.xaml.cs
+++ Grocery.App/Views/ProductView.xaml.cs
@@ -1,12 +1,19 @@
 using Grocery.App.ViewModels;
 
-namespace Grocery.App.Views;
-
-public partial class ProductView : ContentPage
+namespace Grocery.App.Views
 {
-	public ProductView(ProductViewModel viewModel)
-	{
-		InitializeComponent();
-		BindingContext = viewModel;
-	}
+    public partial class ProductView : ContentPage
+    {
+        public ProductView(ProductViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+
+        protected override void OnAppearing() // liet eerst nieuwe producten niet zien, nu wel omdat we elke keer de lijst refreshen bij het tonen
+        {
+            base.OnAppearing();
+            (BindingContext as ProductViewModel)?.LoadProducts();
+        }
+    }
 }
